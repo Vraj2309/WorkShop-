83d3d646c1744ee0b69cbfcd63d479d9c031282b
Vraj2309 <vp2392005@gmail.com>
2025-11-01 11:46:25 +0530

    JS basics

---
+++ REACT/basic.js
@@ -0,0 +1,17 @@
+console.log("Hello World!");
+
+let namee = 'Vraj';
+let age= 20;
+
+console.log(namee)
+console.log(age)
+
+console.log(namee,age);
+console.table([namee,age]);
+
+console.log(typeof namee);
+
+let a=2;
+let b="2";
+console.log(a==b);//== change type of variable
+console.log(a===b);//=== to check both are same or not
